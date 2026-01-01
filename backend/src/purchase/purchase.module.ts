2a8b83b049efe4a2598651104bce7faa4e527d95
Abhijith <abhijith@example.com>
2026-01-02 02:41:31 +0530

    fix: add PurchaseController to PurchaseModule

--- backend/src/purchase/purchase.module.ts
+++ backend/src/purchase/purchase.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PurchaseController } from './purchase.controller';
 import { PurchaseService } from './purchase.service';
 
 @Module({
-  providers: [PurchaseService]
+  controllers: [PurchaseController],
+  providers: [PurchaseService],
 })
-export class PurchaseModule {}
+export class PurchaseModule { }
